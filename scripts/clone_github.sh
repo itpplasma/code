3004509ef3834161621f03310f43a196ba285322
Christopher Albert <albert@alumni.tugraz.at>
2024-01-31 10:25:02 +0100

    Invert check in shell script

--- scripts/clone_github.sh
+++ scripts/clone_github.sh
@@ -2,7 +2,7 @@
 
 REPO=$1
 
-if [ -z "$GIT_HTTPS" ]; then
+if [ -n "$GIT_HTTPS" ]; then
     URL=https://oauth2:$GITHUB_TOKEN@github.com/itpplasma/$REPO
 else
     URL=git@github.com:itpplasma/$REPO
--- scripts/clone_gitlab.sh
+++ scripts/clone_gitlab.sh
@@ -2,7 +2,7 @@
 
 REPO=$1
 
-if [ -z "$GIT_HTTPS" ]; then
+if [ -n "$GIT_HTTPS" ]; then
     URL=https://user:$READ_TOKEN@gitlab.tugraz.at/plasma/codes/$REPO
 else
     URL=git@gitlab.tugraz.at/plasma/codes/$REPO
