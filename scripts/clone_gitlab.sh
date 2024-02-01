16af590c8000f3df04b8b20688b241f26f9d8df7
Christopher Albert <albert@alumni.tugraz.at>
2024-02-01 13:13:50 +0100

    Fix SSH URLs

--- scripts/clone_gitlab.sh
+++ scripts/clone_gitlab.sh
@@ -5,7 +5,7 @@ REPO=$1
 if [ -n "$GIT_HTTPS" ]; then
     URL=https://user:$READ_TOKEN@gitlab.tugraz.at/plasma/codes/$REPO
 else
-    URL=git@gitlab.tugraz.at/plasma/codes/$REPO
+    URL=git@gitlab.tugraz.at:plasma/codes/$REPO.git
 fi
 
 git clone $URL
