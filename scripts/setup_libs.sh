d28abe8173e7a84146f5e3b1c1f17e5c4bd4fee1
Christopher Albert <albert@alumni.tugraz.at>
2024-02-01 17:40:31 +0000

    libs

--- scripts/setup_libs.sh
+++ scripts/setup_libs.sh
@@ -1,9 +1,34 @@
 #!/bin/sh
 
-# If libneo dir doesnt exist, clone it
+cd libs
+
+if [ ! -d "fgsl-1.5.0" ] ; then
+    echo "Fetching and building FGSL..."
+    wget https://doku.lrz.de/files/10746505/10746508/1/1684600947173/fgsl-1.5.0.tar.gz
+    tar xzvf fgsl-1.5.0.tar.gz
+    cd fgsl-1.5.0
+    ./configure
+    make -j4
+    cd ..
+fi
+
+
+if [ ! -d "mfem-4.6" ] ; then
+    echo "Fetching and building MFEM..."
+    wget https://bit.ly/mfem-4-6 -O mfem-4.6.tgz
+    tar xzvf mfem-4.6.tgz
+    cd mfem-4.6
+    mkdir build
+    cd build
+    cmake ..
+    make -j4
+    cd ../..
+fi
+
+
 if [ ! -d "libneo" ] ; then
     echo "Cloning 'libneo'..."
-    scripts/clone_github.sh libneo
+    ../scripts/clone_github.sh libneo
 fi
 
 echo "Building and installing 'libneo'..."
