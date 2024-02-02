cdcfbf311acb486a7f6764c930ff1c54210980f2
Christopher Albert <albert@alumni.tugraz.at>
2024-02-02 12:57:08 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 scripts/setup_venv.sh
 source activate.sh
 
-scripts/setup_fgsl.sh
-scripts/setup_mfem.sh
-scripts/setup_libneo.sh
+cd libs
+../scripts/setup_fgsl.sh
+../scripts/setup_mfem.sh
+../scripts/setup_libneo.sh
+cd ..
