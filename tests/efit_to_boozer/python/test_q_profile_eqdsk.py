dbf756a401a1531fd7863cef67ae4e7fc1641bbc
Christopher Albert <albert@alumni.tugraz.at>
2024-02-02 15:15:08 +0100

    clean

--- tests/efit_to_boozer/python/test_q_profile_eqdsk.py
+++ tests/efit_to_boozer/python/test_q_profile_eqdsk.py
@@ -1,6 +1,7 @@
 """
 Tests for efit_2_boozer and eqdsk_base
 """
+
 import pytest
 
 # Import standard modules
@@ -10,6 +11,7 @@ import os
 # Import modules to be tested
 import efit2boozer
 from libneo import read_eqdsk, FluxLabelConverter
+
 
 def test_q_profile_eqdsk():
 
@@ -29,48 +31,30 @@ def test_q_profile_eqdsk():
     # The safety factor can be calculated alternatively using field line integration
     # this is done as part of the symmetry flux transformation and given as output here
 
-    q_profile_field_line_integration = []  
+    q_profile_field_line_integration = []
 
-    # inp_label = 1 makes it that efit2boozer.magdata_in_symfluxcoord_ext uses the 
-    # flux label si instead of the actual flux psi to determine which 
+    # inp_label = 1 makes it that efit2boozer.magdata_in_symfluxcoord_ext uses the
+    # flux label si instead of the actual flux psi to determine which
     # flux surface one is on. Therefore psi is just set as a dummy variable here.
     inp_label = 1
-    psi = np.array(0.0) 
+    psi = np.array(0.0)
     theta = np.array(0.0)
 
-    #current_directory = os.path.dirname(os.path.realpath(__file__))
+    # current_directory = os.path.dirname(os.path.realpath(__file__))
     current_directory = os.getcwd()
     test_directory = os.path.join(current_directory, "tests/efit_to_boozer/python")
     os.chdir(test_directory)
 
     efit2boozer.efit_to_boozer.init()
     for si in stor_eqdsk:
-        (
-            q,
-            dq_ds,
-            C_norm,
-            dC_norm_ds,
-            sqrtg,
-            bmod,
-            dbmod_dtheta,
-            R,
-            dR_ds,
-            dR_dtheta,
-            Z,
-            dZ_ds,
-            dZ_dtheta,
-            G,
-            dG_ds,
-            dG_dtheta,
-        ) = efit2boozer.magdata_in_symfluxcoord_ext(inp_label, si, psi, theta)
+        (q, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) = (
+            efit2boozer.magdata_in_symfluxcoord_ext(inp_label, si, psi, theta)
+        )
         q_profile_field_line_integration.append(q)
 
     q_profile_field_line_integration = np.array(q_profile_field_line_integration)
 
     os.chdir(current_directory)
 
-    from numpy.testing import assert_allclose
-    assert_allclose(q_profile_field_line_integration, q_profile_eqdsk, rtol=1e-2)
+    np.assert_allclose(q_profile_field_line_integration, q_profile_eqdsk, rtol=1e-2)
     print("Alternative safety factor calculation agrees with EQDSK file within 1%")
-
-    return
