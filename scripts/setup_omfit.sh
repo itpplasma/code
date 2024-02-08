8e539365397bc95287a2736b62be5c6e7250de5b
Christopher Albert <albert@alumni.tugraz.at>
2024-02-08 12:08:28 +0000

    OMFIT licenses, venv

--- scripts/setup_omfit.sh
+++ scripts/setup_omfit.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 git clone https://github.com/gafusion/OMFIT-source.git
 cd OMFIT-source
 git submodule update --init --recursive
 export OMAS_ROOT=$PWD/omas
 export USER=`whoami`
 python -m pip install -e .
+
+mkdir -p $HOME/.LICENSES
+cp $DIR/OMFIT/LICENSES/* $HOME/.LICENSES/
