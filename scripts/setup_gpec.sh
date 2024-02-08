805da266856bc8cc8aade49cefb53e2d4c08cf44
Christopher Albert <albert@alumni.tugraz.at>
2024-02-08 13:50:55 +0000

    GPEC and OMFIT update

---
+++ scripts/setup_gpec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Setting up GPEC..."
+echo "Local use only at ITPcp, not suited for Docker container."
+echo "Works with Intel compiler, not GNU or NVIDIA/PGI."
+echo "Code access: https://princetonuniversity.github.io/GPEC/developers.html"
+
+module load intel/2018.1
+git clone git@github.com:PrincetonUniversity/GPEC.git
+cd GPEC/gpec
+make
--- scripts/setup_omfit.sh
+++ scripts/setup_omfit.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+echo "Setting up OMFIT..."
+echo "Local use only at ITPcp, not suited for Docker container."
+echo "Requires NVIDIA/PGI compiler, not GNU or Intel."
+echo "Code access: https://omfit.io/install.html"
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-git clone https://github.com/gafusion/OMFIT-source.git
+git clone git@github.com:gafusion/OMFIT-source.git
 cd OMFIT-source
 git submodule update --init --recursive
 export OMAS_ROOT=$PWD/omas
