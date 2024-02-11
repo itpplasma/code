f8f0046ea82ab40f1b4a58b3b3ba3d40eda543d2
Christopher Albert <albert@alumni.tugraz.at>
2024-02-11 11:31:33 +0000

    Fix libneo path, filter blob

--- scripts/setup_omfit.sh
+++ scripts/setup_omfit.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 echo "Setting up OMFIT..."
-echo "Local use only at ITPcp, not suited for Docker container."
-echo "Requires NVIDIA/PGI compiler, not GNU or Intel."
+echo "Requires personal Git access to OMFIT repository."
 echo "Code access: https://omfit.io/install.html"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-git clone git@github.com:gafusion/OMFIT-source.git
+git clone --filter=blob:none git@github.com:gafusion/OMFIT-source.git
 cd OMFIT-source
 git submodule update --init --recursive
 export OMAS_ROOT=$PWD/omas
--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ source activate.sh
 cd libs
 ../scripts/setup_fgsl.sh
 ../scripts/setup_mfem.sh
-../scripts/setup_libneo.sh
 cd ..
+
+scripts/setup_libneo.sh
