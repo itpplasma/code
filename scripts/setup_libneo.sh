952eca16b728132ffd7e69cb6f2984a4b59a1fc6
Christopher Albert <albert@alumni.tugraz.at>
2024-02-13 12:42:17 +0100

    Add efit_to_boozer

--- scripts/setup_libneo.sh
+++ scripts/setup_libneo.sh
@@ -5,6 +5,12 @@ if [ ! -d "libneo" ] ; then
     scripts/clone_github.sh libneo
 fi
 
+# TODO: Integrate into libneo
+if [ ! -d "efit_to_boozer" ] ; then
+    echo "Cloning 'efit_to_boozer'..."
+    scripts/clone_github.sh efit_to_boozer
+fi
+
 echo "Building and installing 'libneo'..."
 cd libneo
 ./build.sh
