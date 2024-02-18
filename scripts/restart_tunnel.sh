1f800cc83c7a5d86a18c37495672f59c7618cd88
Christopher Albert <albert@alumni.tugraz.at>
2024-02-18 13:45:46 +0100

    Restarting tunnel

--- scripts/restart_tunnel.sh
+++ scripts/restart_tunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pkill 'code$|code-tunnel$'
-nohup code tunnel --accept-server-license-terms --cli-data-dir /temp/`whoami`/.vscode/`hostname` > /dev/null &
+nohup code tunnel --accept-server-license-terms --cli-data-dir /temp/`whoami`/.vscode/`hostname` < /dev/null &> /dev/null &
