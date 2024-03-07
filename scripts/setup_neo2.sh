1b544a00f7f28a454abd6d94d36255802bfa0668
Christopher Albert <albert@alumni.tugraz.at>
2024-03-07 22:19:04 +0100

    libs replaced by external

--- scripts/setup_neo2.sh
+++ scripts/setup_neo2.sh
@@ -3,13 +3,13 @@ cd NEO-2
 ../scripts/checkout_branch.sh $CI_COMMIT_BRANCH
 # Build NEO-2-PAR
 cd NEO-2-PAR/Build-Debug
-cmake .. -DNEO2_Libs=$CI_PROJECT_DIR/libneo/build -DFGSL_INC=$CI_PROJECT_DIR/libs/fgsl-1.5.0 -DFGSL_LIB=$CI_PROJECT_DIR/libs/fgsl-1.5.0/.libs
+cmake .. -DNEO2_Libs=$CI_PROJECT_DIR/libneo/build -DFGSL_INC=$CI_PROJECT_DIR/external/fgsl-1.5.0 -DFGSL_LIB=$CI_PROJECT_DIR/external/fgsl-1.5.0/.libs
 make
 # Build NEO-2-QL
 cd ../../NEO-2-QL
 mkdir Build
 cd Build
-cmake .. -DNEO2_Libs=$CI_PROJECT_DIR/libneo/build -DFGSL_INC=$CI_PROJECT_DIR/libs/fgsl-1.5.0 -DFGSL_LIB=$CI_PROJECT_DIR/libs/fgsl-1.5.0/.libs
+cmake .. -DNEO2_Libs=$CI_PROJECT_DIR/libneo/build -DFGSL_INC=$CI_PROJECT_DIR/external/fgsl-1.5.0 -DFGSL_LIB=$CI_PROJECT_DIR/external/fgsl-1.5.0/.libs
 make
 
 # TODO Georg
