05d3b9184bec82d19717d47e12dc1eee85b0e67e
Christopher Albert <albert@alumni.tugraz.at>
2024-03-07 18:49:33 +0100

    NetCDF setup

---
+++ scripts/setup_netcdf.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Fetching and building HDF5..."
+curl -L https://github.com/HDFGroup/hdf5/releases/download/hdf5-1_14_3/hdf5-1_14_3.tar.gz -o - | tar xzv
+cd hdfsrc
+./configure --enable-fortran --enable-hl --prefix="$(pwd)/build"
+make -j$(nproc)
+make install
+cd ..
+
+echo "Fetching and building NetCDF..."
+curl -L https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz -o - | tar xzv
+cd netcdf-c-4.9.2
+CPPFLAGS="-I$(pwd)/../hdfsrc/build/include" LDFLAGS="-L$(pwd)/../hdfsrc/build/lib" ./configure --prefix="$(pwd)/build"
+make -j$(nproc)
+cd ..
