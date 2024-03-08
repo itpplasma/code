9e20800712fefa71dedfb0ed12a20a470140326c
Patrick Lainer <patrick.lainer@tugraz.at>
2024-03-08 16:05:23 +0100

    correct NetCDF-Fortran directory

--- scripts/setup_netcdf.sh
+++ scripts/setup_netcdf.sh
@@ -14,7 +14,7 @@ cd netcdf-c-4.9.2
 CPPFLAGS="-I$(pwd)/../hdfsrc/build/include" LDFLAGS="-L$(pwd)/../hdfsrc/build/lib" ./configure --prefix="$(pwd)/build"
 make -j$(nproc)
 make install
-
+cd ..
 
 curl -L https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.1.tar.gz -o - | tar xzv
 cd netcdf-fortran-4.6.1
