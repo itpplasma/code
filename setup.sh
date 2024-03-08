13b4c98ae0b4059cd3e08c7b1df5eafc41518eea
Christopher Albert <albert@alumni.tugraz.at>
2024-03-08 11:31:44 +0100

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export CODE=$SCRIPTPATH
-#source $CODE/scripts/setup_venv.sh
+source $CODE/scripts/setup_venv.sh
 source $CODE/activate.sh
 
-#cd $CODE/external
-#$CODE/scripts/setup_fgsl.sh
+cd $CODE/external
+$CODE/scripts/setup_fgsl.sh
 
 cd $CODE
 source $CODE/scripts/setup_libneo.sh
