2537c3d681dcc4af18465dd91331b7421a269cd9
Christopher Albert <albert@alumni.tugraz.at>
2024-03-08 13:31:30 +0100

    Add stellopt module

--- scripts/setup_stellopt.sh
+++ scripts/setup_stellopt.sh
@@ -5,6 +5,7 @@ if [ ! -d "STELLOPT" ] ; then
     git clone --filter=blob:none https://github.com/PrincetonUniversity/STELLOPT.git STELLOPT
     cd STELLOPT
     export STELLOPT_PATH=$PWD
+    export MYHOME=STELLOPT_PATH/build
     export MACHINE=ubuntu
     ./build_all
     find . -name "*.o" | xargs rm
