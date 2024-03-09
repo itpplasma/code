98f052a6c8384fb74b56f2c3eeb5ddcfb2545331
Christopher Albert <albert@alumni.tugraz.at>
2024-03-09 10:13:18 +0100

    NEO-2 setup with CODE variable

--- scripts/setup_neo2.sh
+++ scripts/setup_neo2.sh
@@ -4,13 +4,13 @@ cd NEO-2
 ../scripts/checkout_branch.sh $CI_MERGE_REQUEST_SOURCE_BRANCH_NAME
 # Build NEO-2-PAR
 cd NEO-2-PAR/Build-Debug
-cmake .. -DNEO2_Libs=$CI_PROJECT_DIR/libneo/build -DFGSL_INC=$CI_PROJECT_DIR/external/fgsl-1.5.0 -DFGSL_LIB=$CI_PROJECT_DIR/external/fgsl-1.5.0/.libs
+cmake .. -DNEO2_Libs=$CODE/libneo/build -DFGSL_INC=$CODE/external/fgsl-1.5.0 -DFGSL_LIB=$CODE/external/fgsl-1.5.0/.libs
 make
 # Build NEO-2-QL
 cd ../../NEO-2-QL
 mkdir Build
 cd Build
-cmake .. -DNEO2_Libs=$CI_PROJECT_DIR/libneo/build -DFGSL_INC=$CI_PROJECT_DIR/external/fgsl-1.5.0 -DFGSL_LIB=$CI_PROJECT_DIR/external/fgsl-1.5.0/.libs
+cmake .. -DNEO2_Libs=$CODE/libneo/build -DFGSL_INC=$CODE/external/fgsl-1.5.0 -DFGSL_LIB=$CODE/external/fgsl-1.5.0/.libs
 make
 
 # TODO Georg
