59960f49dc323548c395134e2c2eef948ca31b80
Christopher Albert <albert@alumni.tugraz.at>
2024-03-10 13:53:35 +0100

    Fix libneo build and activation

--- activate.sh
+++ activate.sh
@@ -5,6 +5,9 @@ export CODE=$SCRIPTPATH
 source $CODE/scripts/util.sh
 add_to_path $CODE/scripts
 export PATH
+add_to_library_path $CODE/libneo/build
+add_to_library_path $CODE/efit_to_boozer/build
+export LD_LIBRARY_PATH
 
 if [ -n "$VIRTUAL_ENV" ]; then
     deactivate > /dev/null 2>&1;
