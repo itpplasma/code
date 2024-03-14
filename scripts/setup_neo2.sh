049f3854ae2058e5f623a2a8ffd126c041a7f9c5
Albert, Christopher <albert@tugraz.at>
2024-03-14 15:38:06 +0100

    Update setup_neo2.sh

--- scripts/setup_neo2.sh
+++ scripts/setup_neo2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 CI_PROJECT_DIR=$CODE
 cd NEO-2
-../scripts/checkout_branch.sh $CODE_BRANCH
+$CODE/scripts/checkout_branch.sh $CODE_BRANCH
+
 # Build NEO-2-PAR
 cd NEO-2-PAR/Build-Debug
 cmake .. -DNEO2_Libs=$CODE/libneo/build -DFGSL_INC=$CODE/external/fgsl-1.5.0 -DFGSL_LIB=$CODE/external/fgsl-1.5.0/.libs
 make
+
 # Build NEO-2-QL
 cd ../../NEO-2-QL
 mkdir Build
@@ -13,8 +15,7 @@ cd Build
 cmake .. -DNEO2_Libs=$CODE/libneo/build -DFGSL_INC=$CODE/external/fgsl-1.5.0 -DFGSL_LIB=$CODE/external/fgsl-1.5.0/.libs
 make
 
-# Installing homebrew python packages for NEO-2 (requires libneo)
+# Installing Python packages for NEO-2 (requires libneo)
 cd ../../..
-scripts/setup_libneo.sh
 cd NEO-2/python
 pip install -e .
