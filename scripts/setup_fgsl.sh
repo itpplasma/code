588093cc6d9fdd74137162630a926c57e41aa648
Christopher Albert <albert@alumni.tugraz.at>
2024-03-14 18:42:20 +0000

    STELLOPT

--- scripts/setup_fgsl.sh
+++ scripts/setup_fgsl.sh
@@ -3,8 +3,8 @@
 if [ ! -d "fgsl-1.5.0" ] ; then
     echo "Fetching and building FGSL..."
     curl -L https://doku.lrz.de/files/10746505/10746508/1/1684600947173/fgsl-1.5.0.tar.gz -o - | tar xzv
-    cd fgsl-1.5.0
+    pushd fgsl-1.5.0
     ./configure
     make
-    cd ..
+    popd
 fi
