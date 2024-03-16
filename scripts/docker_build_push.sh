38a37f17ee4a19dc5ef8a89cd312d1057a80b45e
Christopher Albert <albert@alumni.tugraz.at>
2024-03-16 14:21:29 +0100

    Fix docker push

--- scripts/docker_build_push.sh
+++ scripts/docker_build_push.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/itpplasma/devel-tex -f $CODE/images/$1/Dockerfile . --push
+docker buildx build --platform linux/amd64,linux/arm64 -t ghcr.io/itpplasma/$1 -f $CODE/images/$1/Dockerfile . --push
