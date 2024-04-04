54fca8667e01c13e3e7e5ec4d2c0c6a21d8d2073
Albert, Christopher <albert@tugraz.at>
2024-04-04 10:55:13 +0200

    Update setup_libneo.sh

--- scripts/setup_libneo.sh
+++ scripts/setup_libneo.sh
@@ -5,17 +5,17 @@ if [ ! -d "efit_to_boozer" ] ; then
     echo "Cloning 'efit_to_boozer'..."
     clone_github efit_to_boozer
 fi
-cd efit_to_boozer
-../scripts/checkout_branch.sh $CODE_BRANCH
+pushd efit_to_boozer
+$CODE/scripts/checkout_branch.sh $CODE_BRANCH
 pip install -e .
-cd ..
+popd
 
 echo "Building and installing 'libneo'..."
 if [ ! -d "libneo" ] ; then
     echo "Cloning 'libneo'..."
     clone_github libneo
 fi
-cd libneo
-../scripts/checkout_branch.sh $CODE_BRANCH
+pushd libneo
+$CODE/scripts/checkout_branch.sh $CODE_BRANCH
 pip install -e .
-cd ..
+popd
