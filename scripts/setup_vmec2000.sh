a87035136cfc5d8a28fc7b30889d36604ae1628f
Christopher Albert <albert@alumni.tugraz.at>
2024-05-07 17:30:06 +0000

    Remove fork

--- scripts/setup_vmec2000.sh
+++ scripts/setup_vmec2000.sh
@@ -3,7 +3,7 @@
 
 if [ ! -d "VMEC2000" ] ; then
     echo "Fetching and building VMEC2000..."
-    git clone https://github.com/itpplasma/VMEC2000.git
+    git clone https://github.com/hiddenSymmetries/VMEC2000.git
     pushd VMEC2000
     cp $CODE/scripts/cmake_config_file_vmec2000.json cmake_config_file.json
     cmake -S. -Bbuild -GNinja -DNETCDF_INC_PATH=/usr/include -DSCALAPACK_PREFIX=$CODE/external/scalapack/build -DBLA_VENDOR=OpenBLAS
