49ebce2d201b12a3b5fd4b618a08cc257936a43a
Christopher Albert <albert@alumni.tugraz.at>
2024-05-07 10:08:38 +0000

    vmec fork

--- scripts/setup_vmec2000.sh
+++ scripts/setup_vmec2000.sh
@@ -3,12 +3,13 @@
 
 if [ ! -d "VMEC2000" ] ; then
     echo "Fetching and building VMEC2000..."
-    git clone https://github.com/hiddenSymmetries/VMEC2000.git
+    git clone https://github.com/itpplasma/VMEC2000.git
     pushd VMEC2000
     cmake -S. -Bbuild -GNinja -DNETCDF_INC_PATH=/usr/include -DSCALAPACK_PRE
 FIX=$CODE/external/scalapack/build
     pushd build
     ninja
     popd
+    pip install .
     popd
 fi
