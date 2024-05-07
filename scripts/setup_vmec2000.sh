6d4e2060baa25fabb8678dedd4334ce32cdf6bb9
Christopher Albert <albert@alumni.tugraz.at>
2024-05-07 17:27:35 +0000

    VMEC setup

--- scripts/setup_vmec2000.sh
+++ scripts/setup_vmec2000.sh
@@ -5,11 +5,12 @@ if [ ! -d "VMEC2000" ] ; then
     echo "Fetching and building VMEC2000..."
     git clone https://github.com/itpplasma/VMEC2000.git
     pushd VMEC2000
-    cmake -S. -Bbuild -GNinja -DNETCDF_INC_PATH=/usr/include -DSCALAPACK_PRE
-FIX=$CODE/external/scalapack/build
+    cp $CODE/scripts/cmake_config_file_vmec2000.json cmake_config_file.json
+    cmake -S. -Bbuild -GNinja -DNETCDF_INC_PATH=/usr/include -DSCALAPACK_PREFIX=$CODE/external/scalapack/build -DBLA_VENDOR=OpenBLAS
     pushd build
     ninja
     popd
+    pip install --upgrade numpy f90wrap
     pip install .
     popd
 fi
