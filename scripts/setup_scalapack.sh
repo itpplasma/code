746aa7b6f35b2cdd6156d75dc5ca9f860ae007ea
Christopher Albert <albert@alumni.tugraz.at>
2024-05-10 09:27:06 +0200

    scalapack

--- scripts/setup_scalapack.sh
+++ scripts/setup_scalapack.sh
@@ -3,10 +3,10 @@
 if [ ! -d "scalapack" ] ; then
     echo "Fetching and building ScaLapack..."
     git clone https://github.com/Reference-ScaLAPACK/scalapack.git
-    pushd scalapack
-    cmake -S. -Bbuild -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC
-    pushd build
-    make -j$(nproc)
-    popd
-    popd
 fi
+pushd scalapack
+cmake -S. -Bbuild -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_Fortran_FLAGS=-fPIC -DCMAKE_C_FLAGS=-fPIC
+pushd build
+make -j$(nproc)
+popd
+popd
