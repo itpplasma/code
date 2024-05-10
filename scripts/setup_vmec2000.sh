68d3c6e02a3add5f5b1d5dbe31041117bc205634
Christopher Albert <albert@alumni.tugraz.at>
2024-05-10 09:12:03 +0200

    update setup_vmec2000

--- scripts/setup_vmec2000.sh
+++ scripts/setup_vmec2000.sh
@@ -6,6 +6,7 @@ if [ ! -d "VMEC2000" ] ; then
     git clone https://github.com/hiddenSymmetries/VMEC2000.git
     pushd VMEC2000
     cp $CODE/scripts/cmake_config_file_vmec2000.json cmake_config_file.json
+    sed -i "s|{CODE}|$CODE|g" cmake_config_file.json
     cmake -S. -Bbuild -GNinja -DNETCDF_INC_PATH=/usr/include -DSCALAPACK_PREFIX=$CODE/external/scalapack/build -DBLA_VENDOR=OpenBLAS
     pushd build
     ninja
