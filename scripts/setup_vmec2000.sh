538e09328615416b030b30fb71f42b8ec5e452bc
Christopher Albert <albert@alumni.tugraz.at>
2024-05-10 09:14:17 +0200

    updated vmec setup

--- scripts/setup_vmec2000.sh
+++ scripts/setup_vmec2000.sh
@@ -4,14 +4,15 @@
 if [ ! -d "VMEC2000" ] ; then
     echo "Fetching and building VMEC2000..."
     git clone https://github.com/hiddenSymmetries/VMEC2000.git
-    pushd VMEC2000
-    cp $CODE/scripts/cmake_config_file_vmec2000.json cmake_config_file.json
-    sed -i "s|{CODE}|$CODE|g" cmake_config_file.json
-    cmake -S. -Bbuild -GNinja -DNETCDF_INC_PATH=/usr/include -DSCALAPACK_PREFIX=$CODE/external/scalapack/build -DBLA_VENDOR=OpenBLAS
-    pushd build
-    ninja
-    popd
-    pip install --upgrade numpy f90wrap
-    pip install .
-    popd
 fi
+
+pushd VMEC2000
+cp $CODE/scripts/cmake_config_file_vmec2000.json cmake_config_file.json
+sed -i "s|{CODE}|$CODE|g" cmake_config_file.json
+cmake -S. -Bbuild -GNinja -DNETCDF_INC_PATH=/usr/include -DSCALAPACK_PREFIX=$CODE/external/scalapack/build -DBLA_VENDOR=OpenBLAS
+pushd build
+ninja
+popd
+pip install --upgrade numpy f90wrap
+pip install .
+popd
