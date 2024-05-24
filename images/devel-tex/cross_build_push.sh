a383492e5a8b4dc8ed2be13e74fcf25629569042
Christopher Albert <albert@alumni.tugraz.at>
2024-05-24 08:07:22 +0200

    Docker scripts

--- images/devel-tex/cross_build_push.sh
+++ images/devel-tex/cross_build_push.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker buildx build --platform linux/amd64,linux/arm64 $CODE/images/devel-tex/Dockerfile -t ghcr.io/itpplasma/devel-tex -f . --push
+docker buildx build --platform linux/amd64,linux/arm64 -f $CODE/images/devel-tex/Dockerfile -t ghcr.io/itpplasma/devel-tex . --push
