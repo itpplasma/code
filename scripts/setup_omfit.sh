1d119f5b4cd68989482480cec34bb986865f22db
Christopher Albert <albert@alumni.tugraz.at>
2024-06-20 09:18:24 +0200

    OMFIT

--- scripts/setup_omfit.sh
+++ scripts/setup_omfit.sh
@@ -7,7 +7,7 @@ echo "Code access: https://omfit.io/install.html"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 git clone --filter=blob:none git@github.com:gafusion/OMFIT-source.git
-cd OMFIT-source
+pushd OMFIT-source
 git submodule update --init --recursive
 export OMAS_ROOT=$PWD/omas
 export USER=`whoami`
@@ -15,3 +15,4 @@ python -m pip install -e .
 
 mkdir -p $HOME/.LICENSES
 cp $DIR/OMFIT/LICENSES/* $HOME/.LICENSES/
+popd
