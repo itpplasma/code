46175abf61f95acb0a5dbb1a8d4210f80f033ebe
Hirczy, Andreas <andreas.hirczy@tugraz.at>
2024-06-24 19:25:18 +0200

    setup_compiler_intel.sh: "sudo -v" vorgezogen, sonst Kosmetik

--- scripts/setup_compiler_intel.sh
+++ scripts/setup_compiler_intel.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 echo "Setting up Intel compiler..."
+sudo -v
 
 # download the key to system keyring
 wget -O- https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB \
-| sudo gpg --dearmor | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
+    | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/oneapi-archive-keyring.gpg > /dev/null
 
 # add signed entry to apt sources and configure the APT client to use Intel repository:
-echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" | sudo tee /etc/apt/sources.list.d/oneAPI.list
+echo "deb [signed-by=/usr/share/keyrings/oneapi-archive-keyring.gpg] https://apt.repos.intel.com/oneapi all main" \
+    | sudo tee /etc/apt/sources.list.d/oneAPI.list
 
-sudo apt-get update -y
-sudo apt-get install -y -q --no-install-recommends intel-basekit intel-hpckit
+sudo apt-get update  --quiet --quiet
+sudo apt-get install --quiet --yes --no-install-recommends intel-basekit intel-hpckit
