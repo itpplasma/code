c34965ed4b005417a68c580caf69d00fc6a49404
Hirczy, Andreas <andreas.hirczy@tugraz.at>
2024-06-24 19:28:23 +0200

    Update setup_compiler_nvidia.sh:  - "sudo -v" vorgezogen  - environement module nicht automatisch einbinden - hier wäre mindetsens ein Test gegen mehrfaches anfügen notwendig  - sonst kosmetische Änderungen

--- scripts/setup_compiler_nvidia.sh
+++ scripts/setup_compiler_nvidia.sh
@@ -3,10 +3,22 @@
 echo "Setting up NVIDIA compiler..."
 
 ARCH=$(dpkg --print-architecture)
+sudo -v
 
-curl https://developer.download.nvidia.com/hpc-sdk/ubuntu/DEB-GPG-KEY-NVIDIA-HPC-SDK | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-hpcsdk-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/nvidia-hpcsdk-archive-keyring.gpg] https://developer.download.nvidia.com/hpc-sdk/ubuntu/$ARCH /" | sudo tee /etc/apt/sources.list.d/nvhpc.list
-sudo apt-get update -y
-sudo apt-get install -y nvhpc-24-5
+curl https://developer.download.nvidia.com/hpc-sdk/ubuntu/DEB-GPG-KEY-NVIDIA-HPC-SDK \
+    | gpg --dearmor \
+    | sudo tee /usr/share/keyrings/nvidia-hpcsdk-archive-keyring.gpg > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/nvidia-hpcsdk-archive-keyring.gpg] https://developer.download.nvidia.com/hpc-sdk/ubuntu/$ARCH /" \
+    | sudo tee /etc/apt/sources.list.d/nvhpc.list
 
-echo "module use -a /opt/nvidia/hpc_sdk/modulefiles" >> $HOME/.bashrc
+sudo apt-get update  --quiet --quiet
+sudo apt-get install --quiet --yes nvhpc-24-5
+
+cat <<EOF
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+You might consider to insert the line
+   "module use -a /opt/nvidia/hpc_sdk/modulefiles"
+to your shell initialization file (e.g. ~/.bashrc).
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+EOF
