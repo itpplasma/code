11dc32b5078234b0b60f1d1b78933d611948b585
Christopher Albert <albert@tugraz.at>
2024-06-28 09:11:06 +0200

    Init viper

--- machines/init_viper.sh
+++ machines/init_viper.sh
@@ -7,9 +7,12 @@ module load mkl_parts/1
 module load hdf5-serial/1.14.1
 module load netcdf-serial/4.9.2
 module load gsl/2.7
+module load fftw-serial/3.3.10
 
 export FC=$(which gfortran)
 export CC=$(which gcc)
 export CXX=$(which g++)
 
+export C_INCLUDE_PATH=$FFTW_HOME/include
+export LD_LIBRARY_PATH=$FFTW_HOME/lib:$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$HOME/.local/lib:$LD_LIBRARY_PATH
