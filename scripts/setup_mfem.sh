9fac4307328293e51902470e0790353f25c550b7
Christopher Albert <albert@tugraz.at>
2024-07-02 00:00:34 +0200

    pushd instead of cd

--- scripts/setup_mfem.sh
+++ scripts/setup_mfem.sh
@@ -5,7 +5,6 @@ if [ ! -d "mfem-4.7" ] ; then
     curl -L https://github.com/mfem/mfem/archive/refs/tags/v4.7.tar.gz -o - | tar xz
     mkdir -p mfem-4.7/build
     pushd mfem-4.7/build
-    cd build
     cmake .. -DMFEM_USE_SUITESPARSE=1 -DCMAKE_CXX_FLAGS=-fPIC
     make -j8
     popd
