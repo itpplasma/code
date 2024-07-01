f0a6224745e77403272b9092e883f8fc60d0e5bf
Christopher Albert <albert@tugraz.at>
2024-07-02 00:00:16 +0200

    correct to tar xv

--- scripts/setup_mfem.sh
+++ scripts/setup_mfem.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d "mfem-4.7" ] ; then
     echo "Fetching and building MFEM..."
-    curl -L https://github.com/mfem/mfem/archive/refs/tags/v4.7.tar.gz -o - | tar xv
+    curl -L https://github.com/mfem/mfem/archive/refs/tags/v4.7.tar.gz -o - | tar xz
     mkdir -p mfem-4.7/build
     pushd mfem-4.7/build
     cd build
