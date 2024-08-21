533c17d864b636a8b46eca433f9e3dac25631f8b
Christopher Albert <albert@alumni.tugraz.at>
2024-08-21 13:31:33 +0000

    Upstream GVEC

--- scripts/setup_gvec.sh
+++ scripts/setup_gvec.sh
@@ -6,6 +6,7 @@ GIT_LFS_SKIP_SMUDGE=1 git clone --depth 1 --filter=blob:none -b feature_pygvec_f
     git@gitlab.tugraz.at:plasma/codes/gvec.git
 
 pushd gvec
+git remote add upstream git@gitlab.mpcdf.mpg.de:gvec-group/gvec.git
 cmake --preset gvec_config_release -B build
 cmake --build build -j$(nproc)
 popd
