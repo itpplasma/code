a13dc34e7cb3e9b97808c22c5b3328bd1c001104
Christopher Albert <albert@alumni.tugraz.at>
2024-08-23 10:50:28 +0200

    Add local/bin and local/lib

--- activate.sh
+++ activate.sh
@@ -5,10 +5,12 @@ export CODE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $CODE/scripts/util.sh
 set_branch
 add_to_path $CODE/scripts
+add_to_path $CODE/local/bin
 add_to_path $CODE/bin
 export PATH
 add_to_library_path $CODE/libneo/build
 add_to_library_path $CODE/efit_to_boozer/build
+add_to_library_path $CODE/local/lib
 add_to_library_path $CODE/lib
 export LD_LIBRARY_PATH
 
