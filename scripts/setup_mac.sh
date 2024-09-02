c8a541f4ea8737c98cbda7438ddd49c26e79bc94
Graßler, Georg Sebastian <georg.grassler@tugraz.at>
2024-09-02 10:38:41 +0200

    Update setup_mac.sh

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew install --cask orbstack
 brew install --cask devpod
 brew install --cask visual-studio-code
