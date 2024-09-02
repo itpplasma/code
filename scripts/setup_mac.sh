e3563c6a918f6f320df3bd3ea10200a0c5373c1c
Graßler, Georg Sebastian <georg.grassler@tugraz.at>
2024-09-02 10:46:14 +0200

    Update setup_mac.sh

--- scripts/setup_mac.sh
+++ scripts/setup_mac.sh
@@ -2,6 +2,9 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> $HOME/.zprofile
+eval "$(/usr/local/bin/brew shellenv)"
+
 brew install --cask orbstack
 brew install --cask devpod
 brew install --cask visual-studio-code
