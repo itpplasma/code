a3c5c1c38358cbf3e1853a8d14ced8c44b059c29
Christopher Albert <albert@alumni.tugraz.at>
2024-10-11 14:26:34 +0200

    OMFIT setup

--- scripts/setup_omfit.sh
+++ scripts/setup_omfit.sh
@@ -4,25 +4,20 @@ echo "Setting up OMFIT..."
 echo "Requires personal Git access to OMFIT repository."
 echo "Code access: https://omfit.io/install.html"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
 deactivate
-curl https://pyenv.run | bash
 
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
+pushd $CODE/external
 
-pyenv install 3.7.17
+    git clone --filter=blob:none --recursive -b unstable git@github.com:gafusion/OMFIT-source.git
+    OMFIT-source/install/install.sh
 
-git clone --filter=blob:none git@github.com:gafusion/OMFIT-source.git
-pushd OMFIT-source
-pyenv local 3.7.17
-git submodule update --init --recursive
-export OMAS_ROOT=$PWD/omas
-export USER=`whoami`
-python -m pip install -r install/requirements.txt
-python -m pip install -e .
-mkdir -p $HOME/.LICENSES
-cp $DIR/OMFIT/LICENSES/* $HOME/.LICENSES/
+    pushd OMFIT-source
+        export OMAS_ROOT=$PWD/omas
+        export USER=`whoami`
+        python -m pip install -r install/requirements.txt
+        python -m pip install -e .
+        mkdir -p $HOME/.LICENSES
+        cp $CODE/scripts/OMFIT/LICENSES/* $HOME/.LICENSES/
+    popd
+
 popd
