0f6b0bfc2bf04942d499645babeed437b31b901a
Hirczy, Andreas <andreas.hirczy@tugraz.at>
2024-10-21 11:52:59 +0200

    NVidia-Compiler - environment modules

--- scripts/setup_compiler_nvidia.sh
+++ scripts/setup_compiler_nvidia.sh
@@ -14,11 +14,15 @@ echo "deb [signed-by=/usr/share/keyrings/nvidia-hpcsdk-archive-keyring.gpg] http
 sudo apt-get update  --quiet --quiet
 sudo apt-get install --quiet --yes nvhpc-24-5
 
-cat <<EOF
-
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-You might consider to insert the line
-   "module use -a /opt/nvidia/hpc_sdk/modulefiles"
-to your shell initialization file (e.g. ~/.bashrc).
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-EOF
+# create configuration for "environment modules"
+if [ -d /opt/nvidia/hpc_sdk/modulefiles/ ] ; then
+    if [ -d /usr/share/modules/modulefiles/ ] ; then
+        ln --symbolic --force --verbose /opt/nvidia/hpc_sdk/modulefiles/* \
+           --target-directory=/usr/share/modules/modulefiles/
+        rm -rf /usr/share/modules/modulefiles/nvidia
+    else
+        echo "## ENVIRONMENT Modules - where is the config directory?"
+    fi
+else
+    echo "## ENVIRONMENT Modules - don't know how to configure!"
+fi
