4da11805bbbd188685214b9de7625865f87a99b4
Christopher Albert <albert@alumni.tugraz.at>
2024-10-28 20:26:08 +0100

    Intel 2025 and GPEC update

--- scripts/setup_gpec.sh
+++ scripts/setup_gpec.sh
@@ -3,22 +3,22 @@
 echo "Setting up GPEC..."
 echo "Local use only at ITPcp, not suited for Docker container."
 echo "Works with Intel compiler, not GNU or NVIDIA/PGI."
-echo "Code access: https://princetonuniversity.github.io/GPEC/developers.html"
+echo "Upstream code access: https://princetonuniversity.github.io/GPEC/developers.html"
 
+module load intel/compiler/2025.0.0 intel/mkl/2025.0
 export FC=ifx
 export CC=icx
 export CXX=icx
-export MKLROOT=/opt/intel/oneapi/mkl/2024.2
+export MKLROOT=/opt/intel/oneapi/mkl/2025.0
 
 pushd $CODE/external
-    module load intelcompiler/2024.2
     pushd intel
         source setup_netcdf.sh
     popd
 
-    module netcdf-fortran/4.6.1-intel
+    module load netcdf-fortran/4.6.1-intel
 
-    git clone -b master git@github.com:PrincetonUniversity/GPEC.git
+    git clone -b master git@gitlab.tugraz.at:plasma/codes/gpec.git
     pushd GPEC/install
         OMPFLAG=-qopenmp make
     popd
--- scripts/setup_netcdf.sh
+++ scripts/setup_netcdf.sh
@@ -5,27 +5,31 @@ export CFLAGS=-fPIC
 export CXXFLAGS=-fPIC
 
 echo "Fetching and building HDF5..."
-curl -L https://github.com/HDFGroup/hdf5/releases/download/hdf5-1_14_3/hdf5-1_14_3.tar.gz -o - | tar xz
-mkdir -p hdfsrc/build
-pushd hdfsrc
-sed -i 's/\${CONTENTS}/"\${CONTENTS}"/' config/cmake/HDFUseFortran.cmake
+curl -L https://github.com/HDFGroup/hdf5/releases/download/hdf5_1.14.5/hdf5-1.14.5.tar.gz -o - | tar xz
+mkdir -p hdf5-1.14.5/build
+pushd hdf5-1.14.5
+      sed -i 's/\${CONTENTS}/"\${CONTENTS}"/' config/cmake/HDFUseFortran.cmake
 popd
-pushd hdfsrc/build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) -DBUILD_SHARED_LIBS=ON \
-      -DHDF5_BUILD_FORTRAN=ON ..
-make install
+pushd hdf5-1.14.5/build
+      cmake -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) -DBUILD_SHARED_LIBS=ON \
+            -DHDF5_BUILD_FORTRAN=ON ..
+      make install
 popd
 
 echo "Fetching and building NetCDF..."
 curl -L https://github.com/Unidata/netcdf-c/archive/refs/tags/v4.9.2.tar.gz -o - | tar xz
 pushd netcdf-c-4.9.2
-CPPFLAGS="-I$(pwd)/../hdfsrc/build/include" LDFLAGS="-L$(pwd)/../hdfsrc/build/lib" ./configure --prefix="$(pwd)/build"
-make install
+      CPPFLAGS="-I$(pwd)/../hdf5-1.14.5/build/include" \
+      LDFLAGS="-L$(pwd)/../hdf5-1.14.5/build/lib" \
+      ./configure --prefix="$(pwd)/build"
+      make install
 popd
 
 curl -L https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.6.1.tar.gz -o - | tar xz
 mkdir -p netcdf-fortran-4.6.1/build
 pushd netcdf-fortran-4.6.1/build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) -DNETCDF_C_LIBRARY=$(pwd)/../netcdf-c-4.9.2/build/lib/libnetcdf.so -DNETCDF_C_INCLUDE_DIR=$(pwd)/../netcdf-c-4.9.2/build/include ..
-make install
+      cmake -DCMAKE_INSTALL_PREFIX:PATH=$(pwd) \
+            -DNETCDF_C_LIBRARY=$(pwd)/../netcdf-c-4.9.2/build/lib/libnetcdf.so \
+            -DNETCDF_C_INCLUDE_DIR=$(pwd)/../netcdf-c-4.9.2/build/include ..
+      make install
 popd
