62ce544f27bcb0588aad171b32884580aa663215
Christopher Albert <albert@alumni.tugraz.at>
2024-11-11 15:07:00 +0000

    Moving setup files

--- scripts/setup/linux/setup_debian.sh
+++ scripts/setup/linux/setup_debian.sh
@@ -8,9 +8,9 @@ sudo sed -i -e's/ main$/ main contrib non-free non-free-firmware/g' \
 
 sudo apt-get update -y && apt-get upgrade -y -q --no-install-recommends
 
-sudo $CODE/scripts/setup_base.sh
-sudo $CODE/scripts/setup_interactive.sh
-sudo $CODE/scripts/setup_libs.sh
-sudo $CODE/scripts/setup_octave.sh
-sudo $CODE/scripts/setup_texlive.sh
-sudo $CODE/scripts/setup_fonts.sh
+sudo $CODE/scripts/setup/linux/setup_base.sh
+sudo $CODE/scripts/setup/linux/setup_interactive.sh
+sudo $CODE/scripts/setup/linux/setup_libs.sh
+sudo $CODE/scripts/setup/linux/setup_octave.sh
+sudo $CODE/scripts/setup/linux/setup_texlive.sh
+sudo $CODE/scripts/setup/linux/setup_fonts.sh
