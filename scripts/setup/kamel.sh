8bbae46364a04c58d9d975dab52afc92e533f20c
Markl, Markus <markl@tugraz.at>
2024-11-12 14:07:32 +0100

    Add kamel setup

--- scripts/setup/kamel.sh
+++ scripts/setup/kamel.sh
@@ -20,7 +20,7 @@ fi
 gfortran -c -Wall -Wtabs -mtune=generic $CFLAGS src/*.f
 ar rcs libslatec.a *.o
 rm *.o
-
+mv libslatec.a ../lib/
 echo "Finished building Slatec..."
 
 echo ""
@@ -33,22 +33,18 @@ mkdir build
 cd build
 cmake ..
 make
+cd ../../
 echo "Finished building Sundials..."
-
-
-cd ..
-cd ..
 
 mkdir bessel
 cd bessel
-cp -r ../../KiLCA/math/bessel/* .
+cp -r ../../KAMEL/KiLCA/math/bessel/* .
 cd slatec
 gfortran -c -Wall -Wtabs -mtune=generic -msse2 -mfpmath=sse *.f
 ar rcs libbessel.a *.o
 rm *.o
-mv libbessel.a ../lib/
-cd ..
-cd ..
+mv libbessel.a ../../lib/
+cd ../../
 
 echo "Building lapack..."
 curl -L http://www.netlib.org/lapack/lapack-3.2.1.tgz -o - | tar xz
@@ -57,33 +53,42 @@ cp INSTALL/make.inc.gfortran make.inc
 make lib
 make blaslib
 make clean
-
+cd ..
 echo "Finished building lapack..."
 
 echo "Building gsl-2.4"
-cd ..
 curl -L https://ftp.gnu.org/gnu/gsl/gsl-2.4.tar.gz -o - | tar xz
 cd gsl-2.4
 ./configure
 make
+cd ..
 echo "Finished building gsl-2.4..."
 
 wget https://portal.nersc.gov/project/sparse/superlu/superlu_4.1.tar.gz
 tar -xzvf superlu_4.1.tar.gz
 rm superlu_4.1.tar.gz
 cd SuperLU_4.1
-
+cp MAKE_INC/make.linux make.inc
+sed -i 's/g77/gfortran/g' make.inc
+sed -i "s|SuperLUroot\t= \$(HOME)/Codes/SuperLU_4.1|SuperLUroot = $(pwd)|" make.inc
+make
 
 cd ..
 cd ..
 
-cd KiLCA-QB/KiLCA
+cd KAMEL/KiLCA
 mkdir build
 cd build
 cmake ..
 make
 
-cd ../ql-balance/
+cd ../../KIM/
+mkdir build
+cd build
+cmake ..
+make
+
+cd ../../QL-Balance/
 mkdir build
 cd build
 cmake ..
