5c7aeb6f9549d2300bce23bf2a57e0b7669b3268
Christopher Albert <albert@alumni.tugraz.at>
2024-11-14 09:50:42 +0000

    Devcontainer rework

--- scripts/setup/devcontainer.sh
+++ scripts/setup/devcontainer.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 
-export GIT_HTTPS=1
+export CODE="/workspaces/code"
+cd $CODE
+
 source scripts/setup.sh
 echo 'source /workspaces/code/activate.sh' >> $HOME/.bashrc
 git config --global core.editor 'code --wait'
