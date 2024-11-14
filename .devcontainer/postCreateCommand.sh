aa48053092d7c4799143b0181efed582da867460
Christopher Albert <albert@alumni.tugraz.at>
2024-11-14 16:50:51 +0000

    postCreate

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -4,9 +4,9 @@ CODE_TEMPLATE=/usr/local/src/code_template
 CODE=/workspaces/code
 
 ln -s $CODE_TEMPLATE/.venv $CODE/.venv
-ln -s $CODE_TEMPLATE/efit_to_boozer $CODE/efit_to_boozer
-ln -s $CODE_TEMPLATE/libneo $CODE/libneo
 ln -s $CODE_TEMPLATE/external/fgsl-1.6.0 $CODE/external/fgsl-1.6.0
+cp -r $CODE_TEMPLATE/efit_to_boozer $CODE/efit_to_boozer
+cp -r $CODE_TEMPLATE/libneo $CODE/libneo
 
 echo 'source /workspaces/code/activate.sh' >> $HOME/.bashrc
 git config --global core.editor 'code --wait'
