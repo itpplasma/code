313880f57fa731bde57f29b699e5d91aa45f0e2d
Christopher Albert <albert@alumni.tugraz.at>
2024-11-14 08:39:18 +0100

    scriptpath

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-export CODE=$SCRIPTPATH
+export CODE=$SCRIPTPATH/..
 export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new" 
 
 source $CODE/scripts/setup/venv.sh
