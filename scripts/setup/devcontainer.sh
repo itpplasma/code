c24a98e25e7bdbc61351cd1b15d7dde8f6d5cefd
Christopher Albert <albert@alumni.tugraz.at>
2024-11-14 11:38:52 +0100

    Update devcontainer

--- scripts/setup/devcontainer.sh
+++ scripts/setup/devcontainer.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 
-export CODE="/workspaces/code"
-cd $CODE
+export GIT_HTTPS=1
+export CODE=/workspaces/code
 
+mkdir -p /workspaces
+cd /workspaces
+git clone https://github.com/itpplasma/code.git code
+
+cd $CODE
 source scripts/setup.sh
 echo 'source /workspaces/code/activate.sh' >> $HOME/.bashrc
 git config --global core.editor 'code --wait'
