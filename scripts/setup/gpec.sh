70d72d498360ba053523a95eb54a02477668207c
Christopher Albert <albert@alumni.tugraz.at>
2024-11-21 23:44:29 +0100

    GPEC with GNU on Mac

--- scripts/setup/gpec.sh
+++ scripts/setup/gpec.sh
@@ -19,14 +19,30 @@ else
     export CXX=g++
     export FC=gfortran
     export FFLAGS="-fdefault-real-8 -fdefault-double-8 -fallow-argument-mismatch"
-    export LAPACK_HOME="/usr/include"
-    export NETCDF_FORTRAN_HOME="/usr/include"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        if [[ $(sysctl -n machdep.cpu.brand_string) == *"Apple M1"* ]]; then
+            export LAPACK_HOME="/opt/homebrew/include"
+            export NETCDF_FORTRAN_HOME="/opt/homebrew/include"
+            export CFLAGS="-I/opt/homebrew/include"
+            export FFLAGS="$FFLAGS -I/opt/homebrew/include -L/opt/homebrew/lib"
+            export LDFLAGS="-L/usr/local/lib"
+        else
+            export LAPACK_HOME="/usr/local/include"
+            export NETCDF_FORTRAN_HOME="/usr/local/include"
+            export CFLAGS="-I/usr/local/include"
+            export FFLAGS="$FFLAGS -I/usr/local/include -L/usr/local/lib"
+            export LDFLAGS="-L/usr/local/lib"
+        fi
+    else
+        export LAPACK_HOME="/usr/include"
+        export NETCDF_FORTRAN_HOME="/usr/include"
+    fi
     export OMPFLAG="-fopenmp"
 fi
 
 pushd $CODE/external
     git clone -b master git@gitlab.tugraz.at:plasma/codes/gpec.git GPEC
     pushd GPEC/install
-        make
+        make NETCDFLIBS="-lnetcdff -lnetcdf"
     popd
 popd
