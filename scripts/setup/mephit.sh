5dce71ebd529fa0b9fa8f15261146f28a2432295
Christopher Albert <albert@alumni.tugraz.at>
2024-11-28 17:31:34 +0100

    Update MEPHIT setup

--- scripts/setup/mephit.sh
+++ scripts/setup/mephit.sh
@@ -3,14 +3,14 @@
 set -e
 source /etc/profile.d/modules.sh
 module use -a $CODE/modules
+module load mephit
 
 pushd MEPHIT
-../scripts/checkout_branch.sh $CODE_BRANCH
-mkdir build
-cd build
-export LIBNEO_DIR=$CODE/libneo/build
-module load mfem
-module load fgsl
-cmake ..
-make -j4
+    ../scripts/checkout_branch.sh $CODE_BRANCH
+    mkdir build
+    pushd build
+        cmake ..
+        make -j4
+    popd
+    pip install -e . --no-build-isolation
 popd
