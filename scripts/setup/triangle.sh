9bceaf1bf63e5e1306efdcd811780cb5523b8d76
Christopher Albert <albert@alumni.tugraz.at>
2024-11-29 23:07:10 +0100

    TRIANGLE

--- scripts/setup/triangle.sh
+++ scripts/setup/triangle.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 echo "Fetching and building Triangle..."
-clone_github triangle
-mkdir -p triangle/build
-pushd triangle/build
-    cmake ..
-    make
+git clone https://salsa.debian.org/science-team/triangle.git
+pushd triangle
+    while read -r patch; do
+        patch -p1 < "debian/patches/$patch"
+    done < debian/patches/series
+
+	$CC -I/opt/homebrew/include -O -DTRILIBRARY -fPIC -DPIC -c -o triangle.o triangle.c
+	$CC  -L/opt/homebrew/lib -shared triangle.o -o libtriangle-1.6.so -lm
+	ln -s libtriangle-1.6.so libtriangle.so
+
 popd
