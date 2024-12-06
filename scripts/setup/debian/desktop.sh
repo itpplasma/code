027bc9326bb04173494ae144d7094d06359653e0
Christopher Albert <albert@tugraz.at>
2024-12-06 10:53:17 +0100

    Debian Desktop

---
+++ scripts/setup/debian/desktop.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+url="https://discord.com/api/download?platform=linux&format=deb"
+curl -L -o /tmp/discord.deb $url
+apt install /tmp/discord.deb
+rm -f /tmp/discord.deb
+
+apt install i3
+
+# https://support.mozilla.org/en-US/kb/install-firefox-linux#w_install-firefox-deb-package-for-debian-based-distributions-recommended
+install -d -m 0755 /etc/apt/keyrings
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | tee /etc/apt/preferences.d/mozilla
+apt-get update && apt-get install firefox
+
+
+url="https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
+curl -L -o /tmp/vscode.deb $url
+apt install /tmp/vscode.deb
+rm -f /tmp/vscode.deb
