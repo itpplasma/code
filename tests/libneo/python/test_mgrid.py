d705b6b091505e78efb16c30290db2954d3f9610
Christopher Albert <albert@tugraz.at>
2024-12-13 23:46:57 +0100

    MGRID test modernized

--- tests/libneo/python/test_mgrid.py
+++ tests/libneo/python/test_mgrid.py
@@ -1,33 +1,47 @@
+import pytest
+
 import numpy as np
 
 from libneo.mgrid import MgridFile
+from paths import data_path
+
+testfile_path = data_path / "LHD/VMEC/makegrid_alternative/mgrid_lhd_nfp10.nc"
+
+
+@pytest.fixture
+def mgrid():
+    return MgridFile.from_file(testfile_path)
+
 
 def test_mgrid_read():
-    f = MgridFile.from_file('/proj/plasma/DATA/LHD/albert/vmec/makegrid_alternative/mgrid_lhd_nfp10.nc')
+    f = MgridFile.from_file(testfile_path)
     print(f)
 
-def test_mgrid_read_write():
-    f = MgridFile.from_file('/proj/plasma/DATA/LHD/albert/vmec/makegrid_alternative/mgrid_lhd_nfp10.nc')
-    f.write('/tmp/mgrid_test.nc')
+
+def test_mgrid_read_write(mgrid):
+    mgrid.write("/tmp/mgrid_test.nc")
 
 
-def test_mgrid_read_write_read():
-    f = MgridFile.from_file('/proj/plasma/DATA/LHD/albert/vmec/makegrid_alternative/mgrid_lhd_nfp10.nc')
-    f.write('/tmp/mgrid_test.nc')
-    g = MgridFile.from_file('/tmp/mgrid_test.nc')
+def test_mgrid_read_write_read(mgrid):
+    mgrid.write("/tmp/mgrid_test.nc")
+    g = MgridFile.from_file("/tmp/mgrid_test.nc")
 
     # Assert if all variables are the same
-    assert f.ir == g.ir
-    assert f.jz == g.jz
-    assert f.kp == g.kp
-    assert f.nfp == g.nfp
-    assert f.rmin == g.rmin
-    assert f.zmin == g.zmin
-    assert f.rmax == g.rmax
-    assert f.zmax == g.zmax
-    assert f.mgrid_mode == g.mgrid_mode
-    assert np.array_equal(f.coil_group, g.coil_group)
-    assert np.array_equal(f.coil_current, g.coil_current)
-    assert np.array_equal(f.br, g.br)
-    assert np.array_equal(f.bp, g.bp)
-    assert np.array_equal(f.bz, g.bz)
+    assert mgrid.ir == g.ir
+    assert mgrid.jz == g.jz
+    assert mgrid.kp == g.kp
+    assert mgrid.nfp == g.nfp
+    assert mgrid.rmin == g.rmin
+    assert mgrid.zmin == g.zmin
+    assert mgrid.rmax == g.rmax
+    assert mgrid.zmax == g.zmax
+    assert mgrid.mgrid_mode == g.mgrid_mode
+    assert np.array_equal(mgrid.coil_group, g.coil_group)
+    assert np.array_equal(mgrid.coil_current, g.coil_current)
+    assert np.array_equal(mgrid.br, g.br)
+    assert np.array_equal(mgrid.bp, g.bp)
+    assert np.array_equal(mgrid.bz, g.bz)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-s"])
