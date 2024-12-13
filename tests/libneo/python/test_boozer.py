d6199f98c15a8262f4e9a21f7199e7d0edc5a46d
Christopher Albert <albert@tugraz.at>
2024-12-13 22:35:19 +0100

    Boozer tests

--- tests/libneo/python/test_boozer.py
+++ tests/libneo/python/test_boozer.py
@@ -1,22 +1,31 @@
-# %%
-from libneo import BoozerFile
-from libneo import read_eqdsk, FluxConverter
+import pytest
+
+from pathlib import Path
+import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-trial_boozer_file = "/temp/AG-plasma/archive/2023/andfmar/Boozer_files_perturbation_field/ASDEX_U/30835_rmpm90/out_neo-2_rmp_m90-n0"
-trial_eqdsk_file = "/proj/plasma/DATA/AUG/EQDSK/g30835.3200_ed6"
-bc = BoozerFile(trial_boozer_file)
-eqdsk = read_eqdsk(trial_eqdsk_file)
+from libneo import BoozerFile
+from libneo import read_eqdsk, FluxConverter
+
+data_path = Path(os.environ["DATA"])
+
+trial_boozer_file = data_path / "AUG/BOOZER/30835/out_neo-2_rmp_90-n0"
+trial_eqdsk_file = data_path / "AUG/EQDSK/g30835.3200_ed6"
+bc = BoozerFile(trial_boozer_file.as_posix())
+eqdsk = read_eqdsk(trial_eqdsk_file.as_posix())
 converter = FluxConverter(eqdsk["qprof"])
-no_edges = slice(10,-10)
+no_edges = slice(10, -10)
+
 
 def test_iota():
     stor_bc = bc.s[no_edges]
     iota_bc = np.array(bc.iota[no_edges])
-    stor_eqdsk = converter.sqrtspol2stor(np.sqrt(eqdsk["rho_poloidal"]))
-    iota_eqdsk = np.interp(stor_bc, stor_eqdsk, 1/eqdsk["qprof"])
-    assert np.allclose(iota_eqdsk, -iota_bc, atol=1e-2) # .bc file has only positive itota
+    stor_eqdsk = converter.sqrtspol2stor(np.sqrt(eqdsk["s_pol"]))
+    iota_eqdsk = np.interp(stor_bc, stor_eqdsk, 1 / eqdsk["qprof"])
+    assert np.allclose(
+        iota_eqdsk, -iota_bc, atol=1e-2
+    )  # .bc file has only positive itota
 
 
 def test_get_rho_poloidal():
@@ -24,6 +33,6 @@ def test_get_rho_poloidal():
     sqrtspol_control = converter.stor2sqrtspol(bc.s)[no_edges]
     assert np.allclose(sqrtspol, sqrtspol_control, atol=1e-2)
 
-if __name__=="__main__":
-    test_iota()
-    test_get_rho_poloidal()
+
+if __name__ == "__main__":
+    pytest.main([__file__, "-s"])
