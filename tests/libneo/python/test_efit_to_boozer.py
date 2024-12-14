cbeef6c96dc9dbe384d85035d5e8e173273280ef
Christopher Albert <albert@tugraz.at>
2024-12-14 16:50:49 +0100

    Testing efit_to_boozer

--- tests/libneo/python/test_efit_to_boozer.py
+++ tests/libneo/python/test_efit_to_boozer.py
@@ -12,7 +12,7 @@ from libneo.boozer import get_angles_and_transformation
 from libneo import read_eqdsk, FluxConverter
 
 efit_to_boozer_input = """3600      nstep    - number of integration steps
-500       nlabel   - grid size over radial variable 
+500       nlabel   - grid size over radial variable
 500       ntheta   - grid size over poloidal angle
 10000     nsurfmax - number of starting points for separarix search
 2000      nsurf    - number of flux surfaces in Boozer file
@@ -37,12 +37,12 @@ field_divB0_input = """0                                 ipert        ! 0=eq onl
 @pytest.fixture
 def test_files(code_path, data_path):
     return {
+        "local": code_path / "libneo/test/resources/input_efit_file.dat",
         "CHEASE": data_path / "DEMO/EQDSK/Equilibrium_DEMO2019_CHEASE/MOD_Qprof_Test/EQDSK_DEMO2019_q1_COCOS_02.OUT",
         "AUG": data_path / "AUG/EQDSK/g30835.3200_ed6",
         "MASTU": data_path / "MASTU/EQDSK/MAST_47051_450ms.geqdsk",
-        "local": code_path / "libneo/test/resources/input_efit_file.dat",
-        "PROCESS": data_path / "DEMO/EQDSK/Equil_2021_PMI_QH_mode_betap_1d04_li_1d02_Ip_18d27MA_SOF.eqdsk",
-        "standardized": data_path / "DEMO/EQDSK/Equil_2021_PMI_QH_mode_betap_1d04_li_1d02_Ip_18d27MA_SOF_std.eqdsk",
+        # TODO: "PROCESS": data_path / "DEMO/EQDSK/Equil_2021_PMI_QH_mode_betap_1d04_li_1d02_Ip_18d27MA_SOF.eqdsk",
+        # TODO: "standardized": data_path / "DEMO/EQDSK/Equil_2021_PMI_QH_mode_betap_1d04_li_1d02_Ip_18d27MA_SOF_std.eqdsk",
     }
 
 
@@ -63,49 +63,47 @@ def test_angles_and_transformation(test_files):
 
 
 @pytest.mark.slow
-def test_q_profile_eqdsk(data_path):
+def test_q_profile_eqdsk(test_files):
 
-    # Read data from EQDSK file
-    basedir_eqdsk = data_path / "DEMO/EQDSK/Equilibrium_DEMO2019_CHEASE"
-    eqfile_path = basedir_eqdsk / "MOD_Qprof_Test/EQDSK_DEMO2019_q1_COCOS_02.OUT"
+    for key in ["CHEASE"]:
+        # TODO: local, AUG, MASTU, PROCESS, standardized
+        test_file = test_files[key]
+        print(f"Testing {key} EQDSK file: {test_file}")
+        tmp_path = init_run_path(test_file)
+        print(f"Running in {tmp_path}")
+        os.chdir(tmp_path)
+        eqdsk_data = read_eqdsk(str(test_file))
 
-    eqdsk_data = read_eqdsk(str(eqfile_path))
+        # The data in EQDSK is writting in poloidal flux label,
+        # but efit_to_boozer uses toroidal flux label. Therefore, we need a mapping
+        q_profile_eqdsk = eqdsk_data["qprof"]
+        spol_eqdsk = np.linspace(0.0, 1.0, q_profile_eqdsk.shape[0])
+        converter = FluxConverter(q_profile_eqdsk)
+        stor_eqdsk = converter.spol2stor(spol_eqdsk)
 
-    # The data in EQDSK is writting in poloidal flux label,
-    # but efit_to_boozer uses toroidal flux label. Therefore, we need a mapping
-    q_profile_eqdsk = eqdsk_data["qprof"]
-    spol_eqdsk = np.linspace(0.0, 1.0, q_profile_eqdsk.shape[0])
-    converter = FluxConverter(q_profile_eqdsk)
-    stor_eqdsk = converter.spol2stor(spol_eqdsk)
+        # The safety factor can be calculated alternatively using field line integration
+        # this is done as part of the symmetry flux transformation and given as output here
 
-    # The safety factor can be calculated alternatively using field line integration
-    # this is done as part of the symmetry flux transformation and given as output here
+        q_profile_field_line_integration = []
 
-    q_profile_field_line_integration = []
+        # inp_label = 1 makes it that efit_to_boozer.magdata_in_symfluxcoord_ext uses the
+        # flux label si instead of the actual flux psi to determine which
+        # flux surface one is on. Therefore psi is just set as a dummy variable here.
+        inp_label = 1
+        psi = np.array(0.0)
+        theta = np.array(0.0)
 
-    # inp_label = 1 makes it that efit_to_boozer.magdata_in_symfluxcoord_ext uses the
-    # flux label si instead of the actual flux psi to determine which
-    # flux surface one is on. Therefore psi is just set as a dummy variable here.
-    inp_label = 1
-    psi = np.array(0.0)
-    theta = np.array(0.0)
+        efit_to_boozer.efit_to_boozer.init()
+        for si in stor_eqdsk:
+            (q, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) = (
+                efit_to_boozer.magdata_in_symfluxcoord_ext(inp_label, si, psi, theta)
+            )
+            q_profile_field_line_integration.append(q)
 
-    print(f"Testing EQDSK file: {eqfile_path}")
-    tmp_path = init_run_path(eqfile_path)
-    print(f"Running in {tmp_path}")
-    os.chdir(tmp_path)
+        q_profile_field_line_integration = np.array(q_profile_field_line_integration)
 
-    efit_to_boozer.efit_to_boozer.init()
-    for si in stor_eqdsk:
-        (q, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _) = (
-            efit_to_boozer.magdata_in_symfluxcoord_ext(inp_label, si, psi, theta)
-        )
-        q_profile_field_line_integration.append(q)
-
-    q_profile_field_line_integration = np.array(q_profile_field_line_integration)
-
-    assert_allclose(q_profile_field_line_integration, q_profile_eqdsk, rtol=1e-2)
-    print("Alternative safety factor calculation agrees with EQDSK file within 1%")
+        assert_allclose(q_profile_field_line_integration, q_profile_eqdsk, rtol=1e-2)
+        print("Alternative safety factor calculation agrees with EQDSK file within 1%")
 
 
 def init_run_path(gfile):
