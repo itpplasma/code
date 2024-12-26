73932ce0ffead7c68703a3697989372bf6fd4807
Christopher Albert <albert@tugraz.at>
2024-12-26 16:24:54 +0100

    venv

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,7 @@ export CODE=$SCRIPTPATH/..
 export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new"
 
 $CODE/scripts/setup/venv.sh
+source $CODE/.venv/bin/activate
 
 pushd $CODE/external
     $CODE/scripts/setup/fgsl.sh
