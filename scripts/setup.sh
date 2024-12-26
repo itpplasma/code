0c349c37c63ec62a4aad2c032622ebe8e6821eca
Christopher Albert <albert@tugraz.at>
2024-12-26 16:21:08 +0100

    Setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+set -e
+
 SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 export CODE=$SCRIPTPATH/..
-export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new" 
+export GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=accept-new"
 
-source $CODE/scripts/setup/venv.sh
-source $CODE/activate.sh
+$CODE/scripts/setup/venv.sh
 
 pushd $CODE/external
-$CODE/scripts/setup/fgsl.sh
+    $CODE/scripts/setup/fgsl.sh
+    $CODE/scripts/setup/triangle.sh
 popd
 
 pushd $CODE
-source $CODE/scripts/setup/libneo.sh
+    $CODE/scripts/setup/libneo.sh
 popd
 
 source $CODE/activate.sh
