16f8a246bc1e22f97597c5178b44b9953c4088a3
Christopher Albert <albert@alumni.tugraz.at>
2025-01-14 13:04:37 +0100

    shell improved

--- shell.nix
+++ shell.nix
@@ -8,17 +8,26 @@ mkShell {
     ninja
     gcc
     gfortran
+    mpi
 
     # Libs
     openblas
     suitesparse
     hdf5
     netcdf
+    netcdffortran
 
     # Interactive
     coreutils
     which
     less
     vim
+    lazygit
   ];
+
+  shellHook = ''
+    export CC=gcc
+    export CXX=g++
+    export FC=gfortran
+  '';
 }
