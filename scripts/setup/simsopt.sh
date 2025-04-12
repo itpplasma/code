3058eed146e8a5523d76f3d1c442ee08c69171a5
Christopher Albert <albert@tugraz.at>
2025-04-12 12:59:16 +0200

    simsopt in external

--- scripts/setup/simsopt.sh
+++ scripts/setup/simsopt.sh
@@ -1,5 +1,6 @@
-
 #!/usr/bin/env bash
+
+cd $CODE/external
 
 if [ ! -d "simsopt" ] ; then
     echo "Fetching and building simsopt..."
--- scripts/setup/stellopt.sh
+++ scripts/setup/stellopt.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
+pushd $CODE/external
+
 if [ ! -d "STELLOPT" ] ; then
     echo "Fetching and building STELLOPT..."
     git clone --filter=blob:none https://github.com/PrincetonUniversity/STELLOPT.git STELLOPT
-    pushd STELLOPT
-    export STELLOPT_PATH=$PWD
-    export MYHOME=STELLOPT_PATH/build
-    export MACHINE=ubuntu
-    bash build_all
-    find . -name "*.o" | xargs rm
-    popd
 fi
+    pushd STELLOPT
+        export STELLOPT_PATH=$PWD
+        export MYHOME=STELLOPT_PATH/build
+        export MACHINE=ubuntu
+        bash build_all
+        find . -name "*.o" | xargs rm
+    popd
+popd
