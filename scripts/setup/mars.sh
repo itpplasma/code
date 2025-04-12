cbba72a7b62abd1d0a091ea6ef43cd3757ea3b57
Christopher Albert <albert@tugraz.at>
2025-04-12 11:50:45 +0200

    MARS and OMFIT

--- scripts/setup/mars.sh
+++ scripts/setup/mars.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -e
-module load intel/ifort intel/mkl intel/mpi
+module load intel/compiler-rt/2024.2.1 intel/ifort intel/mpi
 cd $CODE/external/intel
 git clone git@gitlab.tugraz.at:plasma/codes/mars.git MARS
 cd MARS/MarsQ_2022
--- scripts/setup/omfit.sh
+++ scripts/setup/omfit.sh
@@ -7,7 +7,10 @@ echo "Code access: https://omfit.io/install.html"
 deactivate
 
 export USER=`whoami`
-git clone --filter=blob:none --recursive -b unstable git@gitlab.tugraz.at:plasma/codes/OMFIT-source.git
-OMFIT-source/install/install.sh
+cd $CODE/external
+git clone --filter=blob:none -b unstable git@gitlab.tugraz.at:plasma/codes/OMFIT-source.git
+cd OMFIT-source
+git submodule update --init omas
+install/install.sh
 mkdir -p $HOME/.LICENSES
 cp $CODE/scripts/setup/omfit/LICENSES/* $HOME/.LICENSES/
