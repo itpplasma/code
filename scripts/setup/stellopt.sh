8e80ffd81785491fa604cb138f1445123ec51de3
Christopher Albert <albert@alumni.tugraz.at>
2025-06-20 15:26:35 +0200

    STELLOPT install on mac

--- scripts/setup/stellopt.sh
+++ scripts/setup/stellopt.sh
@@ -9,8 +9,24 @@ fi
     pushd STELLOPT
         export STELLOPT_PATH=$PWD
         export MYHOME=STELLOPT_PATH/build
-        export MACHINE=ubuntu
+        select_machine
         bash build_all
         find . -name "*.o" | xargs rm
     popd
 popd
+
+select_machine() {
+    case "$(uname -s)" in
+        Darwin)
+            cp $CODE/scripts/setup/stellopt/make_osx_brew_m1.inc $CODE/external/STELLOPT/SHARE
+            export MACHINE=osx_brew_m1
+            ;;
+        Linux)
+            export MACHINE=ubuntu
+            ;;
+        *)
+            echo "Unknown OS: $(uname -s)" >&2
+            export MACHINE=unknown
+            ;;
+    esac
+}
