a2eb5989f7bc1799858c29481823c45d0219aaba
Christopher Albert <albert@tugraz.at>
2025-07-07 17:02:29 +0200

    Mac scripts

--- scripts/setup/mac-desktop.sh
+++ scripts/setup/mac-desktop.sh
@@ -2,6 +2,7 @@
 
 brew install --cask claude google-chrome nextcloud
 brew install --cask orbstack visual-studio-code
+brew install --cask lm-studio
 
 # sudo ln -s /Applications/OrbStack.app/Contents/MacOS/xbin/docker /usr/local/bin/docker
 # sudo systemsetup -setremotelogin on
--- scripts/setup/mac.sh
+++ scripts/setup/mac.sh
@@ -2,6 +2,6 @@
 
 brew install gcc make cmake ninja python python-tk
 brew install gsed autoconf automake autogen git lazygit
-brew install openblas suitesparse metis
+brew install openblas suitesparse metis gsl
 brew install hdf5 netcdf netcdf-fortran fftw graphviz
 brew install node vim tmux htop
