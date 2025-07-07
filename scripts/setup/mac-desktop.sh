57d76f1bb250e944ee422f631ddff971e4ee674e
Christopher Albert <albert@tugraz.at>
2025-07-07 07:55:22 -0700

    Mac Desktop

---
+++ scripts/setup/mac-desktop.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+brew install --cask claude google-chrome nextcloud
+brew install --cask orbstack visual-studio-code
+
+# sudo ln -s /Applications/OrbStack.app/Contents/MacOS/xbin/docker /usr/local/bin/docker
+# sudo systemsetup -setremotelogin on
--- scripts/setup/mac.sh
+++ scripts/setup/mac.sh
@@ -1,22 +1,7 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> $HOME/.zprofile
-eval "$(/usr/local/bin/brew shellenv)"
-
-brew install gcc make cmake ninja python@3.11 python-tk@3.11
+brew install gcc make cmake ninja python python-tk
 brew install gsed autoconf automake autogen git lazygit
 brew install openblas suitesparse metis
 brew install hdf5 netcdf netcdf-fortran fftw graphviz
-brew install --cask orbstack devpod visual-studio-code
-
-sudo ln -s /Applications/OrbStack.app/Contents/MacOS/xbin/docker /usr/local/bin/docker
-
-mkdir -p $HOME/Nextcloud/plasma
-ln -s $HOME/.devpod/agent/contexts/default/workspaces/code/content $HOME/code
-
-docker pull ghcr.io/itpplasma/devcontainer:latest
-
-devpod provider add docker
-devpod up git@github.com:itpplasma/code --id code --ide vscode
+brew install node vim tmux htop
