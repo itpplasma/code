dff7416f90cc486dc99492519a522d15c91ca13d
Christopher Albert <albert@tugraz.at>
2025-07-07 17:36:47 +0200

    mac wget

--- scripts/setup/mac.sh
+++ scripts/setup/mac.sh
@@ -4,7 +4,7 @@ brew install gcc make cmake ninja python python-tk
 brew install gsed autoconf automake autogen git lazygit
 brew install openblas suitesparse metis gsl
 brew install hdf5 netcdf netcdf-fortran fftw graphviz
-brew install node vim tmux htop
+brew install node vim tmux htop wget
 brew install modules
 
 echo "source /opt/homebrew/opt/modules/init/zsh" >> $HOME/.zshrc
