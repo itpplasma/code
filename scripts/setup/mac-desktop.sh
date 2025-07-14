252ea6388a8a1e7838e9a6800891b9efe3c359fb
Christopher Albert <albert@tugraz.at>
2025-07-14 10:56:23 +0200

    Mac Desktop

--- scripts/setup/mac-desktop.sh
+++ scripts/setup/mac-desktop.sh
@@ -4,6 +4,7 @@ brew install --cask claude google-chrome nextcloud
 brew install --cask orbstack visual-studio-code
 brew install --cask lm-studio
 brew install --cask discord
+brew install --cask xquartz
 
 # sudo ln -s /Applications/OrbStack.app/Contents/MacOS/xbin/docker /usr/local/bin/docker
 # sudo systemsetup -setremotelogin on
