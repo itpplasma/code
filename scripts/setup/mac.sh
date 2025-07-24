f10e81dca8f17a6c7d39b281db2cbcd7dc7fda70
Christopher Albert <albert@tugraz.at>
2025-07-24 13:49:53 +0200

    petsc on mac

--- scripts/setup/mac.sh
+++ scripts/setup/mac.sh
@@ -2,8 +2,8 @@
 
 brew install gcc make cmake ninja python python-tk
 brew install gsed autoconf automake autogen git lazygit
+brew install open-mpi hdf5-mpi netcdf netcdf-fortran fftw graphviz
 brew install openblas suitesparse metis gsl petsc
-brew install hdf5 netcdf netcdf-fortran fftw graphviz
 brew install node vim tmux htop wget gcovr lcov
 brew install modules tree mc ripgrep bc
 brew install texlive
