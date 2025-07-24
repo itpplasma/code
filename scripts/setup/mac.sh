191024777c2b9bf5fe53b91c3f6bcc12c8cbe3b8
Christopher Albert <albert@tugraz.at>
2025-07-24 13:48:26 +0200

    petsc on mac

--- scripts/setup/mac.sh
+++ scripts/setup/mac.sh
@@ -2,9 +2,10 @@
 
 brew install gcc make cmake ninja python python-tk
 brew install gsed autoconf automake autogen git lazygit
-brew install openblas suitesparse metis gsl
+brew install openblas suitesparse metis gsl petsc
 brew install hdf5 netcdf netcdf-fortran fftw graphviz
 brew install node vim tmux htop wget gcovr lcov
 brew install modules tree mc ripgrep bc
+brew install texlive
 
 echo "source /opt/homebrew/opt/modules/init/zsh" >> $HOME/.zshrc
