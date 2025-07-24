3011dbe3e9c13db5350b8a8baf4c0489d49c1093
Christopher Albert <albert@tugraz.at>
2025-07-24 17:42:42 +0200

    mac

--- scripts/setup/mac.sh
+++ scripts/setup/mac.sh
@@ -6,6 +6,6 @@ brew install open-mpi hdf5-mpi netcdf netcdf-fortran fftw graphviz
 brew install openblas suitesparse metis gsl petsc
 brew install node vim tmux htop wget gcovr lcov
 brew install modules tree mc ripgrep bc
-brew install texlive
+brew install texlive poppler
 
 echo "source /opt/homebrew/opt/modules/init/zsh" >> $HOME/.zshrc
