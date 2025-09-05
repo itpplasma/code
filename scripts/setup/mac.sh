acbb3f33ed2f8411c9eca5157c4670c25a26950c
Christopher Albert <albert@tugraz.at>
2025-09-05 11:50:54 +0200

    pytest on mac

--- scripts/setup/mac.sh
+++ scripts/setup/mac.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
-brew install gcc make cmake ninja python python-tk
+brew install gcc make cmake ninja python python-tk pytest
 brew install gsed autoconf automake autogen git lazygit
 brew install open-mpi hdf5-mpi netcdf netcdf-fortran fftw graphviz
 brew install openblas suitesparse metis gsl petsc swig
 brew install node vim tmux htop wget gcovr lcov
 brew install modules tree mc ripgrep bc
-brew install texlive poppler pandoc
+brew install texlive poppler pandoc biber
 brew install chezmoi age zinit fzf zoxide
 
 echo "source /opt/homebrew/opt/modules/init/zsh" >> $HOME/.zshrc
