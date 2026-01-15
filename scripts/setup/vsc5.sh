07d67baf3b772b1f619b58bcf1b7ef1513a49218
Christopher Albert <albert@tugraz.at>
2026-01-15 16:09:15 +0100

    python setup vsc

--- scripts/setup/vsc5.sh
+++ scripts/setup/vsc5.sh
@@ -21,10 +21,10 @@ module load gcc/12.2.0-gcc-9.5.0-ohbahza &>/dev/null
 module load mkl/2022.0.1 &>/dev/null
 module load netcdf-fortran/4.6.0-gcc-12.2.0-ad7kryt &>/dev/null
 module load fftw/3.3.10-gcc-12.2.0-jstgwxm &>/dev/null
-module load python/3.12.8-gcc-12.2.0-4y5tbpr &>/dev/null
 module load cmake/3.31.6-gcc-12.2.0-b55yivf &>/dev/null
 module load boost/1.80.0-gcc-12.2.0-3fphho7 &>/dev/null
 module load gmp/6.2.1-gcc-12.2.0-hmbnkk4 &>/dev/null
+module load python/3.12.8-gcc-12.2.0-4y5tbpr &>/dev/null
 
 # Activate CODE environment (quietly)
 source $HOME/code/activate.sh &>/dev/null
