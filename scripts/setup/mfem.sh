9f0d01eeaee16b2fa64e734e2c3edeee7b27cd48
Patrick Lainer <patrick.lainer@tugraz.at>
2026-09-24 16:12:47 +0200

    use most recent version of MFEM

--- scripts/setup/mfem.sh
+++ scripts/setup/mfem.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-if [ ! -d "mfem-4.7" ] ; then
+version=4.10
+if [ ! -d "mfem-$version" ] ; then
     echo "Fetching and building MFEM..."
-    curl -L https://github.com/mfem/mfem/archive/refs/tags/v4.7.tar.gz -o - | tar xz
-    mkdir -p mfem-4.7/build
-    pushd mfem-4.7/build
+    curl -L "https://github.com/mfem/mfem/archive/refs/tags/v$version.tar.gz" -o - | tar xz
+    mkdir -p "mfem-$version/build"
+    pushd "mfem-$version/build"
     cmake .. -DMFEM_USE_SUITESPARSE=1 -DCMAKE_CXX_FLAGS=-fPIC
     make -j8
     popd
